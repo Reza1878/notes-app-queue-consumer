87b25f44ee9a95ab3a7c5fc4c966aad3bae430e2
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-09-19 21:24:34 +0700

    Update: change node mailer config

--- src/MailSender.js
+++ src/MailSender.js
@@ -3,8 +3,7 @@ const nodemailer = require('nodemailer');
 class MailSender {
   constructor() {
     this._transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      service: process.env.SMTP_SERVICE,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASSWORD,
